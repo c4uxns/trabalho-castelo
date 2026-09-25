068149f0008088fe1d290c2cd3b0cb20b25b3a4c
cauan <cauanmedeiros193@gmail.com>
2026-09-24 21:19:54 -0400

    refactor: comment out health route in app.py and remove restart policy in docker-compose.yml; update index.html with team members

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/health')
-def health():
-    return {"status": "healthy"}, 200
+# @app.route('/health')
+# def health():
+#     return {"status": "healthy"}, 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
